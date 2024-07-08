f4c9d06a311884757cd2f1b9fdec121d04921ef5
James Mayclin <maycj@amazon.com>
2024-07-08 18:15:57 +0000

    add resumption bench

--- bench_test.go
+++ bench_test.go
@@ -3,116 +3,11 @@ package susgobench
 import (
 	"crypto/tls"
 	"log"
-	"net"
 	"os"
 	"testing"
-	"time"
 )
 
-func runServer(config *tls.Config) error {
-
-	listener, err := tls.Listen("tcp", "localhost:8443", config)
-	if err != nil {
-		return err
-	}
-	defer listener.Close()
-
-	log.Println("Server is listening on localhost:8443")
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("failed to accept connection: %v", err)
-			continue
-		}
-
-		tlsConn := conn.(*tls.Conn)
-		tlsConn.Handshake()
-		state := conn.(*tls.Conn).ConnectionState()
-		log.Printf("Negotiated cipher suite: %s", tls.CipherSuiteName(state.CipherSuite))
-		tlsConn.Close()
-		//go handleConnection(conn)
-	}
-}
-
-// func TestLocalHostHandshake(t *testing.T) {
-// 	serverConfig := tlsServerConfig()
-// 	clientConfig := tlsClientConfig()
-
-// 	listener, err := tls.Listen("tcp", "127.0.0.1:8444", serverConfig)
-// 	if err != nil {
-// 		t.Fatalf("failed to listen: %v", err)
-// 	}
-// 	defer listener.Close()
-
-// 	go func() {
-// 		for {
-// 			conn, err := listener.Accept()
-// 			if err != nil {
-// 				log.Printf("failed to accept connection: %v", err)
-// 				continue
-// 			}
-// 			go func() {
-// 				tlsConn := conn.(*tls.Conn)
-// 				if err := tlsConn.Handshake(); err != nil {
-// 					log.Printf("server handshake failed: %v", err)
-// 				}
-// 				log.Println(tlsConn.ConnectionState())
-// 				conn.Close()
-// 			}()
-// 		}
-// 	}()
-
-// 	time.Sleep(500 * time.Millisecond)
-
-// 	conn, err := tls.Dial("tcp", "127.0.0.1:8444", clientConfig)
-// 	if err != nil {
-// 		t.Fatalf("client handshake failed: %v", err)
-// 	}
-// 	conn.Close()
-// }
-
-// func SharedMemTest(b *testing.B) {
-// 	serverConfig := tlsServerConfig()
-// 	clientConfig := tlsClientConfig()
-
-// 	b.ResetTimer()
-
-// 	for i := 0; i < b.N; i++ {
-// 		clientToServer := &bytes.Buffer{}
-// 		serverToClient := &bytes.Buffer{}
-
-// 		clientConn := tls.Client(&dummyConn{r: serverToClient, w: clientToServer}, clientConfig)
-// 		serverConn := tls.Server(&dummyConn{r: clientToServer, w: serverToClient}, serverConfig)
-
-// 		b.Log("making the channel")
-// 		done := make(chan bool)
-
-// 		go func() {
-// 			defer close(done)
-// 			if err := serverConn.Handshake(); err != nil {
-// 				b.Log("server handshake failed")
-// 				//b.Fatalf("server handshake failed: %v", err)
-// 			}
-// 		}()
-
-// 		if err := clientConn.Handshake(); err != nil {
-// 			b.Fatalf("client handshake failed: %v", err)
-// 		}
-
-// 		<-done
-
-// 		clientConn.Close()
-// 		serverConn.Close()
-// 	}
-// }
-
 func BenchmarkSharedMemHandshake(b *testing.B) {
-	err := os.Setenv("GODEBUG", "tls=1")
-	if err != nil {
-		log.Fatalf("failed to set GODEBUG environment variable: %v", err)
-	}
-
 	serverConfig := tlsServerConfig()
 	clientConfig := tlsClientConfig()
 
@@ -146,8 +41,49 @@ func BenchmarkSharedMemHandshake(b *testing.B) {
 			b.Fatal("handshake was not complete")
 		}
 		//log.Printf("Negotiated cipher suite: %s", tls.CipherSuiteName(state.CipherSuite))
-
 		clientDone = true
+		clientConn.Close()
+	}
+
+}
+
+func BenchmarkSharedMemResumption(b *testing.B) {
+	serverConfig := resumptionServerConfig()
+	clientConfig := resumptionClientConfig()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+
+		clientToServer := make(chan []byte)
+		serverToClient := make(chan []byte)
+
+		clientDone := false
+		serverDone := false
+
+		clientConn := tls.Client(newDummyConn("client", serverToClient, clientToServer, &clientDone), clientConfig)
+		serverConn := tls.Server(newDummyConn("server", clientToServer, serverToClient, &serverDone), serverConfig)
+
+		//done := make(chan bool)
+
+		go func() {
+			if err := serverConn.Handshake(); err != nil {
+				b.Logf("server handshake failed: %v", err)
+			}
+			serverDone = true
+			serverConn.Close()
+		}()
+
+		if err := clientConn.Handshake(); err != nil {
+			b.Fatalf("client handshake failed: %v", err)
+		}
+		state := clientConn.ConnectionState()
+		if !state.HandshakeComplete {
+			b.Fatal("handshake was not complete")
+		}
+		readBuf := make([]byte, 1)
+		clientDone = true
+		clientConn.Read(readBuf)
+		//log.Println("client is closing")
 		clientConn.Close()
 	}
 
@@ -159,8 +95,8 @@ func TestSharedMemHandshake(t *testing.T) {
 		log.Fatalf("failed to set GODEBUG environment variable: %v", err)
 	}
 
-	serverConfig := tlsServerConfig()
-	clientConfig := tlsClientConfig()
+	serverConfig := resumptionServerConfig()
+	clientConfig := resumptionClientConfig()
 
 	clientToServer := make(chan []byte)
 	serverToClient := make(chan []byte)
@@ -181,7 +117,16 @@ func TestSharedMemHandshake(t *testing.T) {
 			t.Log((err))
 			//b.Fatalf("server handshake failed: %v", err)
 		}
+		readBuf := make([]byte, 1)
+		n, _ := serverConn.Read(readBuf)
+		if n != 0 {
+			t.Log("ERROR: unexpected read")
+		}
+		// writeBuf := make([]byte, 1)
+		// clientConn.Write(writeBuf)
+
 		serverDone = true
+		log.Println("server is closing")
 		serverConn.Close()
 	}()
 
@@ -192,78 +137,10 @@ func TestSharedMemHandshake(t *testing.T) {
 	if !state.HandshakeComplete {
 		t.Fatal("handshake was not complete")
 	}
-
+	readBuf := make([]byte, 1)
+	log.Println("getting the session ticket")
 	clientDone = true
+	clientConn.Read(readBuf)
+	//log.Println("client is closing")
 	clientConn.Close()
 }
-
-type dummyConn struct {
-	name    string
-	readCh  chan []byte
-	writeCh chan []byte
-	readBuf []byte
-	closing *bool
-}
-
-func newDummyConn(name string, readCh, writeCh chan []byte, closing *bool) *dummyConn {
-	return &dummyConn{
-		name:    name,
-		readCh:  readCh,
-		writeCh: writeCh,
-		closing: closing,
-	}
-}
-
-func (c *dummyConn) Read(b []byte) (n int, err error) {
-	//log.Printf("READ %s", c.name)
-	if len(c.readBuf) == 0 {
-		c.readBuf = <-c.readCh
-	}
-	n = copy(b, c.readBuf)
-	c.readBuf = c.readBuf[n:]
-	//log.Printf("READ %s done", c.name)
-	return n, nil
-}
-
-func (c *dummyConn) Write(b []byte) (n int, err error) {
-	//log.Printf("WRITE %s", c.name)
-	if *c.closing {
-		// pretend the write was successful
-		//log.Printf("WRITE %s special done", c.name)
-		return len(b), nil
-
-	}
-	c.writeCh <- b
-	//log.Printf("WRITE %s done", c.name)
-
-	return len(b), nil
-}
-
-func (c *dummyConn) Close() error {
-	return nil
-}
-
-func (c *dummyConn) LocalAddr() net.Addr {
-	return dummyAddr{}
-}
-
-func (c *dummyConn) RemoteAddr() net.Addr {
-	return dummyAddr{}
-}
-
-func (c *dummyConn) SetDeadline(t time.Time) error {
-	return nil
-}
-
-func (c *dummyConn) SetReadDeadline(t time.Time) error {
-	return nil
-}
-
-func (c *dummyConn) SetWriteDeadline(t time.Time) error {
-	return nil
-}
-
-type dummyAddr struct{}
-
-func (dummyAddr) Network() string { return "dummy" }
-func (dummyAddr) String() string  { return "dummy" }
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package susgobench
 
 import (
+	"crypto/rand"
 	"crypto/tls"
 	"crypto/x509"
 	"log"
+	"net"
 	"os"
+	"time"
 )
 
 const (
@@ -24,6 +27,28 @@ func tlsServerConfig() *tls.Config {
 	}
 }
 
+func resumptionServerConfig() *tls.Config {
+
+	// Generate session ticket keys
+	sessionTicketKeys := make([][32]byte, 1)
+	if _, err := rand.Read(sessionTicketKeys[0][:]); err != nil {
+		log.Fatalf("failed to generate session ticket key: %v", err)
+	}
+
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		log.Fatalf("failed to load key pair: %v", err)
+	}
+
+	config := tls.Config{
+		Certificates:           []tls.Certificate{cert},
+		SessionTicketKey:       sessionTicketKeys[0],
+		SessionTicketsDisabled: false,
+	}
+	//config.SetSessionTicketKeys(sessionTicketKeys)
+	return &config
+}
+
 func tlsClientConfig() *tls.Config {
 	caCert, err := os.ReadFile(caFile)
 	if err != nil {
@@ -39,4 +64,129 @@ func tlsClientConfig() *tls.Config {
 		RootCAs:    caCertPool,
 		ServerName: "localhost",
 	}
+}
+
+func resumptionClientConfig() *tls.Config {
+	caCert, err := os.ReadFile(caFile)
+	if err != nil {
+		log.Fatalf("failed to read CA certificate: %v", err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	if !caCertPool.AppendCertsFromPEM(caCert) {
+		log.Fatalf("failed to add CA certificate to pool")
+	}
+
+	return &tls.Config{
+		RootCAs:                caCertPool,
+		ClientSessionCache:     NewLoggingSessionCache(128),
+		ServerName:             "localhost",
+		SessionTicketsDisabled: false,
+	}
+}
+
+type dummyConn struct {
+	name    string
+	readCh  chan []byte
+	writeCh chan []byte
+	readBuf []byte
+	closing *bool
+}
+
+func newDummyConn(name string, readCh, writeCh chan []byte, closing *bool) *dummyConn {
+	return &dummyConn{
+		name:    name,
+		readCh:  readCh,
+		writeCh: writeCh,
+		closing: closing,
+	}
+}
+
+func (c *dummyConn) Read(b []byte) (n int, err error) {
+	//log.Printf("READ %s with %d len", c.name, len(b))
+	if *c.closing {
+		// return fake 1 byte, to avoid fighting with their weird
+		// io
+		//log.Printf("READ %s special done", c.name)
+		return 0, net.ErrClosed
+	}
+	if len(c.readBuf) == 0 {
+		c.readBuf = <-c.readCh
+	}
+	n = copy(b, c.readBuf)
+	c.readBuf = c.readBuf[n:]
+	//log.Printf("READ %s done with %d bytes", c.name, n)
+	return n, nil
+}
+
+func (c *dummyConn) Write(b []byte) (n int, err error) {
+	//log.Printf("WRITE %s %d bytes", c.name, len(b))
+	if *c.closing {
+		// pretend the write was successful
+		//log.Printf("WRITE %s special done", c.name)
+		return len(b), nil
+
+	}
+	c.writeCh <- b
+	//log.Printf("WRITE %s done", c.name)
+
+	return len(b), nil
+}
+
+func (c *dummyConn) Close() error {
+	return nil
+}
+
+func (c *dummyConn) LocalAddr() net.Addr {
+	return dummyAddr{}
+}
+
+func (c *dummyConn) RemoteAddr() net.Addr {
+	return dummyAddr{}
+}
+
+func (c *dummyConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (c *dummyConn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+func (c *dummyConn) SetWriteDeadline(t time.Time) error {
+	return nil
+}
+
+type dummyAddr struct{}
+
+func (dummyAddr) Network() string { return "dummy" }
+func (dummyAddr) String() string  { return "dummy" }
+
+type loggingSessionCache struct {
+	cache tls.ClientSessionCache
+}
+
+func NewLoggingSessionCache(size int) tls.ClientSessionCache {
+	return &loggingSessionCache{
+		cache: tls.NewLRUClientSessionCache(size),
+	}
+}
+
+func (l *loggingSessionCache) Put(key string, cs *tls.ClientSessionState) {
+	//fmt.Printf("Adding session with key: %s\n", key)
+	l.cache.Put(key, cs)
+}
+
+func (l *loggingSessionCache) Get(key string) (*tls.ClientSessionState, bool) {
+	session, ok := l.cache.Get(key)
+	if ok {
+		//fmt.Printf("Retrieved session with key: %s\n", key)
+	} else {
+		//fmt.Printf("No session found with key: %s\n", key)
+	}
+	return session, ok
+}
+
+func main() {
+	log.Println("hello world")
 }
